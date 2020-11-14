d7d7513f9ce0bcd7b4f5248b025ed78bdaca7234
michalgrzegor <michalgrzegor@gmail.com>
2020-11-14 08:28:21 +0100

    added toggle buttons

--- src/components/buttons/buttonsContainer.tsx
+++ src/components/buttons/buttonsContainer.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MoviesContext } from '../../contexts/moviesContext';
+import { MoviesContextInterface } from '../../interfaces/moviesContextInterface';
 import Button from './button';
 
 import './buttons.css';
 
 const ButtonsContainer: React.FC = () => {
+  const {
+    moviesState: { noMoreMovies, isMatch },
+  } = useContext(MoviesContext) as MoviesContextInterface;
+
   return (
     <div className="btnContainer">
-      <Button btnType="like" />
-      <Button btnType="dislike" />
+      {!noMoreMovies && !isMatch && (
+        <div>
+          <Button btnType="like" />
+          <Button btnType="dislike" />
+        </div>
+      )}
     </div>
   );
 };
